198f2e37d0e3324858df4d994c6602a1ffbb307e
rafaelva-z <rafaelvaz.art@gmail.com>
2024-06-22 23:20:46 +0100

    merged Deployment #35 from main to rafa

--- app.py
+++ app.py
@@ -20,25 +20,29 @@ from plots import make_plots, querry_bq
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './sublime-lyceum-426907-r9-353181f6f35f.json'
 
+
 def get_now():
     from datetime import datetime as dt
 
     return dt.now().isoformat()
+
 
 def hash_password(password):
     from hashlib import sha256
 
     return sha256((password + "tiny pinch of salt").encode("utf-8")).hexdigest()
 
+
 @app.route("/")
 def index():
     return render_template('index.html', isLoginPage=False, isAuthenticated=session.get("isAuthenticated", False))
+
 
 @app.route('/run', methods=['GET', 'POST'])
 def run():
     if not session.get("isAuthenticated", False):
         return redirect(url_for('login'))
-    
+
     google_map_api_key = os.getenv('GOOGLE_MAP_API_KEY')
 
     plots = make_plots()
@@ -48,28 +52,30 @@ def run():
 
     filtered_df_2024 = df_historic_data[df_historic_data['Year'] == 2024]
     filtered_df_2024 = filtered_df_2024.sort_values(by="Designacao")
-    columns_to_drop = ["Latitude", "Longitude", "Year", "Month", "Day", "Localidade_Postal", "Freguesia", "Codigo_do_Ponto_de_Atendimento", "Population_Density", "Codigo_Freguesia", "Data"]
+    columns_to_drop = ["Latitude", "Longitude", "Year", "Month", "Day", "Localidade_Postal",
+                       "Freguesia", "Codigo_do_Ponto_de_Atendimento", "Population_Density", "Codigo_Freguesia", "Data"]
     filtered_df_2024 = filtered_df_2024.drop(columns=columns_to_drop)
     # print(filtered_df_2024.head(100))
 
     # group the rows by designacao, averaging the other elements
 
     df_grouped = filtered_df_2024.groupby('Designacao').agg({"Procuras": 'mean',
-        "Atendimentos": 'mean',
-        "Desistencias": 'mean',
-        "Tempo_medio_de_espera_diario": 'mean',
-        "Population": 'mean',
-        }).reset_index()
+                                                             "Atendimentos": 'mean',
+                                                             "Desistencias": 'mean',
+                                                             "Tempo_medio_de_espera_diario": 'mean',
+                                                             "Population": 'mean',
+                                                             }).reset_index()
 
     # Calculate the stress values
-    
-    stress_value = [ int((a['Procuras'] * a['Desistencias'] * a['Tempo_medio_de_espera_diario'])
-                         / (a['Atendimentos'] * a ['Population'])) 
-                for a in json.loads(df_grouped.to_json(orient='records'))]
+
+    stress_value = [int((a['Procuras'] * a['Desistencias'] * a['Tempo_medio_de_espera_diario'])
+                        / (a['Atendimentos'] * a['Population']))
+                    for a in json.loads(df_grouped.to_json(orient='records'))]
 
     # Dispose of unecessary data
 
-    columns_to_drop = ["Procuras", "Atendimentos", "Desistencias", "Tempo_medio_de_espera_diario", "Population"]
+    columns_to_drop = ["Procuras", "Atendimentos", "Desistencias",
+                       "Tempo_medio_de_espera_diario", "Population"]
     df_grouped = df_grouped.drop(columns=columns_to_drop)
     df_grouped['stress_value'] = stress_value
 
@@ -80,12 +86,14 @@ def run():
     predictions = create_dataframe_with_random_deviation(original_df)
     loc1 = predictions['Location']
     to_merge_to_cards_table = predictions.sort_values(by='Location')
-    columns_to_drop = ["Year", "Procuras","Tempo_medio_de_espera_diario", "Atendimentos" ,"Desistencias"]
-    to_merge_to_cards_table = to_merge_to_cards_table.drop(columns=columns_to_drop)
-    to_merge_to_cards_table = to_merge_to_cards_table.groupby('Location').agg({"necessity_metric": 'mean'}).reset_index()
+    columns_to_drop = ["Year", "Procuras",
+                       "Tempo_medio_de_espera_diario", "Atendimentos", "Desistencias"]
+    to_merge_to_cards_table = to_merge_to_cards_table.drop(
+        columns=columns_to_drop)
+    to_merge_to_cards_table = to_merge_to_cards_table.groupby(
+        'Location').agg({"necessity_metric": 'mean'}).reset_index()
     # print(to_merge_to_cards_table.head(3))
-    
-    
+
     cards_table = []
     js = json.loads(df_grouped.to_json())
     js2 = json.loads(to_merge_to_cards_table.to_json())
@@ -99,7 +107,15 @@ def run():
 
     items = [f'Item {i}' for i in range(1, 3)]  # Example list of items
     # return render_template('index.html', items=items)
-    return render_template('run.html', isLoginPage=False, isAuthenticated=session.get("isAuthenticated", False), google_map_api_key=google_map_api_key, items=items, graph_html=plots, cards_data=cards_table)
+    return render_template(
+        'run.html',
+        isLoginPage=False,
+        isAuthenticated=session.get("isAuthenticated", False),
+        google_map_api_key=google_map_api_key,
+        items=items,
+        graph_html=plots,
+        cards_data=cards_table
+    )
 
 
 # this is not used as of now
@@ -140,17 +156,21 @@ def create_dataframe_with_random_deviation(original_data: pd.DataFrame) -> pd.Da
     new_data = original_data.copy()
 
     for column in ['Procuras', 'Tempo_medio_de_espera_diario', 'Desistencias', 'Atendimentos']:
-        new_data[column] = new_data[column] * (1 + np.random.uniform(-percentage_deviation_large, percentage_deviation_large, size=new_data.shape[0]))
+        new_data[column] = new_data[column] * \
+            (1 + np.random.uniform(-percentage_deviation_large,
+             percentage_deviation_large, size=new_data.shape[0]))
 
-    new_data['necessity_metric'] = new_data['necessity_metric'] * (1 + np.random.uniform(-percentage_deviation_small, percentage_deviation_small, size=new_data.shape[0]))
+    new_data['necessity_metric'] = new_data['necessity_metric'] * \
+        (1 + np.random.uniform(-percentage_deviation_small,
+         percentage_deviation_small, size=new_data.shape[0]))
     return new_data
 
 
 @app.route('/predict', methods=['POST'])
-def predict():  
+def predict():
     original_df = pd.read_csv('/static/assets/model_frame.csv')
     predictions = create_dataframe_with_random_deviation(original_df)
-  
+
     return jsonify(predictions)
 
 
@@ -178,11 +198,11 @@ def login():
 
     return render_template('login.html', isLoginPage=True, isAuthenticated=session.get("isAuthenticated", False))
 
+
 @app.route('/logout', methods=['GET'])
 def logout():
     session["isAuthenticated"] = False
     return redirect(url_for('index'))
-
 
 
 @app.route("/profile")
@@ -219,13 +239,12 @@ def report():
         return render_template('error.html', isLoginPage=False, isAuthenticated=session.get("isAuthenticated", False), error="Failed to retrieve data"), response.status_code
 
 
-
 @app.route('/save-report', methods=['POST'])
 def save_report():
     if not session.get("isAuthenticated", False):
         return redirect(url_for('login'))
     if request.method == 'POST':
-       print(request.form)
+        print(request.form)
 
 
 if __name__ == '__main__':
